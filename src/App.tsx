4f66aab75ec01e81da70022ce915b67001d164b4
DmitryZagr <bikemaster2000@list.ru>
2020-04-02 21:32:13 +0300

    initial

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,17 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import { RouteComponentProps } from "react-router-dom";
+
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  useParams,
+  useLocation
+} from "react-router-dom";
+
 function App() {
   return (
     <div className="App">
@@ -23,4 +34,95 @@ function App() {
   );
 }
 
-export default App;
+function BasicExample() {
+
+let query = useQuery();
+  
+console.log(query);
+  
+let name : string = "123";
+
+console.log(name);
+
+  return (
+    <Router>
+      <div>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+          <li>
+            <Link to="/oauth20/callback">callback</Link>
+          </li>
+        </ul>
+
+        <hr />
+
+        {/*
+          A <Switch> looks through all its children <Route>
+          elements and renders the first one whose path
+          matches the current URL. Use a <Switch> any time
+          you have multiple routes, but you want only one
+          of them to render at a time
+        */}
+        <Switch>
+          <Route exact path="/" component = {Home} />
+          <Route path="/about"  component = {About} />
+          <Route path="/oauth20/callback" component = {Callback}>
+            {/* let name = {useQuery().get("name")}; */}
+            {/* <Callback name =  {name}  /> */}
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  );
+}
+
+// You can think of these components as "pages"
+// in your app.
+
+function Home() {
+  return (
+    <div>
+      <h2>Home</h2>
+    </div>
+  );
+}
+
+function About() {
+  return (
+    <div>
+      <h2>About</h2>
+    </div>
+  );
+}
+
+function Callback() {
+
+  // const queryString = require('query-string');
+
+  
+  // const params = new URLSearchParams(props.location.search);
+  // const foo = params.get('foo'); // bar
+
+  // console.log(this.props.params); // should print "param1=value1&param2=value2...."
+
+
+  return (
+    <div>
+      <h2>callback + " " + </h2>
+    </div>
+  );
+}
+
+// A custom hook that builds on useLocation to parse
+// the query string for you.
+function useQuery() {
+  return new URLSearchParams(useLocation().search);
+}
+
+
+export default BasicExample;
